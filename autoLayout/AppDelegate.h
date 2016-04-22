07c2123696d6be41fdc5395714f707cb9bae70a1
JackRen <zhb@zhbdemac-mini-2.local>
2016-04-22 09:19:37 -0700

    Initial Commit

---
+++ autoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autoLayout
+//
+//  Created by JackRen on 16/4/22.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autoLayout
+//
+//  Created by JackRen on 16/4/22.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
