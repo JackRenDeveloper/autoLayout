51acdd09f371910d81af4610371c53305e96b028
JackRenDeveloper <jackren_developer@163.com>
2016-05-12 17:18:52 -0700

    Auto

---
+++ autoLayout/myTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  myTableViewCell.h
+//  autoLayout
+//
+//  Created by JackRen on 16/4/22.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myTableViewCell : UITableViewCell
+
+@end
---
+++ autoLayout/myTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  myTableViewCell.m
+//  autoLayout
+//
+//  Created by JackRen on 16/4/22.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import "myTableViewCell.h"
+
+@implementation myTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
